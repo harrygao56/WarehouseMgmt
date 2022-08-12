fc59b5109c8308bc761c340ee68a1a70d8a801d5
harrygao56 <harrygao56@gmail.com>
2022-08-12 14:14:05 -0500

    Changed Late Fee Method

--- WarehouseMgmt/src/LateFee.java
+++ WarehouseMgmt/src/LateFee.java
@@ -21,9 +21,7 @@ import javax.swing.SwingConstants;
 public class LateFee extends JPanel {
 	private GUI gui;
 
-	private JTextField percField;
-	private JTextField minField;
-	private JTextField maxField;
+	private JTextField amount;
 
 	public LateFee(GUI gui) {
 		this.gui = gui;
@@ -38,50 +36,18 @@ public class LateFee extends JPanel {
 		title.setFont(new Font("Trebuchet MS", Font.BOLD, 19));
 		title.setPreferredSize(new Dimension(300, 45));
 
-		JLabel perc = new JLabel("Percentage of Balance: ", SwingConstants.CENTER);
+		JLabel perc = new JLabel("Fee Amount: ", SwingConstants.CENTER);
 		perc.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
 		perc.setPreferredSize(new Dimension(200, 20));
 
-		percField = new JTextField();
-		percField.setPreferredSize(new Dimension(120, 25));
-		percField.addKeyListener(new KeyAdapter() {
+		amount = new JTextField();
+		amount.setPreferredSize(new Dimension(120, 25));
+		amount.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent ke) {
 				if ((ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') || ke.getKeyChar() == '\u0008') {
-					percField.setEditable(true);
+					amount.setEditable(true);
 				} else {
-					percField.setEditable(false);
-				}
-			}
-		});
-		
-		JLabel min = new JLabel("Fee Minimum: ", SwingConstants.CENTER);
-		min.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		min.setPreferredSize(new Dimension(170, 20));
-		
-		minField = new JTextField();
-		minField.setPreferredSize(new Dimension(120, 25));
-		minField.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent ke) {
-				if ((ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') || ke.getKeyChar() == '\u0008') {
-					minField.setEditable(true);
-				} else {
-					minField.setEditable(false);
-				}
-			}
-		});
-		
-		JLabel max = new JLabel("Fee Maximum: ", SwingConstants.CENTER);
-		max.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		max.setPreferredSize(new Dimension(170, 20));
-		
-		maxField = new JTextField();
-		maxField.setPreferredSize(new Dimension(120, 25));
-		maxField.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent ke) {
-				if ((ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') || ke.getKeyChar() == '\u0008') {
-					maxField.setEditable(true);
-				} else {
-					maxField.setEditable(false);
+					amount.setEditable(false);
 				}
 			}
 		});
@@ -152,27 +118,7 @@ public class LateFee extends JPanel {
 		gbc.gridx = 1;
 		gbc.gridy = 1;
 		gbc.anchor = GridBagConstraints.WEST;
-		add(percField, gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.anchor = GridBagConstraints.EAST;
-		add(min, gbc);
-
-		gbc.gridx = 1;
-		gbc.gridy = 2;
-		gbc.anchor = GridBagConstraints.WEST;
-		add(minField, gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		gbc.anchor = GridBagConstraints.EAST;
-		add(max, gbc);
-
-		gbc.gridx = 1;
-		gbc.gridy = 3;
-		gbc.anchor = GridBagConstraints.WEST;
-		add(maxField, gbc);
+		add(amount, gbc);
 
 		gbc.gridx = 0;
 		gbc.gridy = 4;
@@ -193,27 +139,14 @@ public class LateFee extends JPanel {
 
 	public void submit() {
 		ArrayList<String[]> l = SQLFunctions.getTenantBasicInfoAndBalance();
-		double percent = Integer.valueOf(percField.getText())/100.0;
-		int min = Integer.valueOf(minField.getText());
-		int max = Integer.valueOf(maxField.getText());
+		int val = Integer.valueOf(amount.getText());
 		
 		for (int i = 0; i < l.size(); i++) {
 			int tenant = Integer.valueOf(l.get(i)[0]);
 			
 			if (Integer.valueOf(l.get(i)[3]) > 0) {
-				int fee = (int)(percent*Integer.valueOf(l.get(i)[3]));
-				if (fee < min) {
-					SQLFunctions.applyFee(tenant, min);
-					SQLFunctions.newHistory(tenant, min, "Late Fee", null);
-				}
-				else if (fee > max) {
-					SQLFunctions.applyFee(tenant, max);
-					SQLFunctions.newHistory(tenant, max, "Late Fee", null);
-				}
-				else {
-					SQLFunctions.applyFee(tenant, fee);
-					SQLFunctions.newHistory(tenant, fee, "Late Fee", null);
-				}
+				SQLFunctions.applyFee(tenant, val);
+				SQLFunctions.newHistory(tenant, val, "Late Fee", null);
 			}
 		}
 		
